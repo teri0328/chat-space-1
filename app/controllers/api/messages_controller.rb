23de22c02b49ad4074ecaaea6538bb5dc6c9c30f
SAWANO10 <key.strawberry.1000@gmail.com>
2019-08-07 13:23:23 +0900

    Auto-Update3

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,8 @@
 class Api::MessagesController < ApplicationController
   def index
-    @group = Group.find(params[:group_id]) 
-    @messages = @group.messages.includes(:user).where('id > ?', params[:id])
     
+    @messages = Message.where('id > ?', params[:id]).where(params[:group_id]).includes(:user)
+  
     respond_to do |format| 
       format.html 
       format.json
