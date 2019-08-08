6b0324903ded5392dfbd4eedf78c5db0163ac62f
SAWANO10 <key.strawberry.1000@gmail.com>
2019-08-08 18:17:53 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen 3000
 
 
 stderr_path "#{app_path}/log/unicorn.stderr.log"
